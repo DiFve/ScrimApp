0427d44a99f68b4f74be75708b916a277f8d4439
DiFve <jkblade6@gmail.com>
2021-11-25 00:49:49 +0700

    add getuserInfo

--- YIMPSApiServer/serverYIMPS/urls.py
+++ YIMPSApiServer/serverYIMPS/urls.py
@@ -1,20 +1,26 @@
 from django.urls import path
 from . import views
-from .routes import postRoute,teamRoute
+from .routes import postRoute,teamRoute,profileRoute
 
 urlpatterns = [
     path('', views.index , name ='index'),
     path('getallposts',postRoute.getAllPost,name='getPost'),
     path('createpost',postRoute.createPost,name = 'createPost'),
     path('request-to-scrim/<str:pk>',postRoute.reqToScrim,name='reqToScrim'),
-    path('createUser',profileRoute.creteProfile,name='creteProfile'),
+    path('getposts-sorted',postRoute.getPostSort,name='getPostSort'),
+
     path('getpost/<str:pk>',postRoute.getPostById,name='getPostById'),
     #teamRoute
     path('createteam',teamRoute.createTeam,name='createTeam'),
     path('getteam/<str:pk>',teamRoute.getTeam,name='getTeam'),
     path('addmember/<str:pk>',teamRoute.addMember,name='addMember'),
-    path('getposts-sorted',postRoute.getPostSort,name='getPostSort'),
+    
+
+    #profileRoute
+    path('createUser',profileRoute.creteProfile,name='creteProfile'),
     path('login',profileRoute.login,name='login'),
     path('editProfile',profileRoute.editProfile,name='editProfile'),
+    path('getUserInfo/<str:pk>',profileRoute.getUserInfo,name='getUserInfo')
+
 ]
 
