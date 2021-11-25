6e691bb4001d49ab958eac1db5b681523fef6146
kawjaw66 <kawjaw66@gmail.com>
2021-11-25 17:25:42 +0700

    +accept req +changed create post

--- YIMPSApiServer/serverYIMPS/urls.py
+++ YIMPSApiServer/serverYIMPS/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .routes import postRoute,teamRoute
+from .routes import postRoute,teamRoute,profileRoute
 
 urlpatterns = [
     path('', views.index , name ='index'),
@@ -9,11 +9,13 @@ urlpatterns = [
     path('request-to-scrim/<str:pk>',postRoute.reqToScrim,name='reqToScrim'),
     path('createUser',profileRoute.creteProfile,name='creteProfile'),
     path('getpost/<str:pk>',postRoute.getPostById,name='getPostById'),
+    path('getposts-sorted',postRoute.getPostSort,name='getPostSort'),
+    path('test-averagerank/<str:pk>',postRoute.getAvgRank,name='getAvgRank'),
+    path('post/<str:pk>/accept-request',postRoute.acceptReq,name='acceptReq'),
     #teamRoute
     path('createteam',teamRoute.createTeam,name='createTeam'),
     path('getteam/<str:pk>',teamRoute.getTeam,name='getTeam'),
     path('addmember/<str:pk>',teamRoute.addMember,name='addMember'),
-    path('getposts-sorted',postRoute.getPostSort,name='getPostSort'),
     path('login',profileRoute.login,name='login'),
     path('editProfile',profileRoute.editProfile,name='editProfile'),
 ]
